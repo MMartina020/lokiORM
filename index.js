c0036c89fa5d4ac495b2a672788051521c390280
MMartina020 <marcsikaa15@gmail.com>
2022-03-10 13:01:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("LokiJS");
+
+const loki = require("lokijs");
+
+const db = new loki('adatok.data');
+
+/**
+hal{
+    -id
+    -halfaj
+    -tomeg (gramm)
+}
+ */
+
+db.loadDatabase({}, () => {
+    let halaim = db.getCollection("halak");
+    if (halaim === null) {
+        halaim = db.getCollection("halak");
+    }
+
+
+    const elsoHal = {
+        id: 2, //uuid kellene
+        halfaj: "csuka", //halfaj
+        tomeg: 1900
+    }
+
+    halaim.insert(elsoHal);
+
+    /**
+    collection memória
+    db -> fizikai cucc(file itt)
+    */
+
+    const pontyok = halaim.find({halfaj:"ponty"});
+    console.table(pontyok);
+
+    const ponty = halaim.find({halfaj:"ponty"});
+    console.log(ponty);
+
+    halaim.remove(ponty);
+
+    db.saveDatabase((err) => {
+
+        if (!err) {
+            return console.log("Mentés sikeres");
+        }
+
+        //hiba ág
+    }); //Hát nem így 
+});
